2be2f521690fe4f011793d37de82d3255284ff05
xolani-mokJS <70220609+xolani-mokJS@users.noreply.github.com>
2022-08-17 15:40:17 +0200

    js
    js updated

---
+++ script.js
@@ -0,0 +1,19 @@
+
+// creating new list item 
+function newItemElement(){
+    let listItem = document.createElement("li");
+    let myInput = document.getElementById('myInput');
+    let myInputValue = myInput.value;
+
+    let txt = document.createTextNode(myInputValue);
+    listItem.appendChild(txt);
+
+    if (myInputValue === ''){
+        alert('Please write something to add on list');
+    } else {
+        document.getElementById('myList').appendChild(listItem);
+    }
+
+    document.getElementById('myInput').value = '';
+}
+
