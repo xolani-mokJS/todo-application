1876f7eeb0c51434e99e71ac436a84a203c2da1f
xolani-mokJS <70220609+xolani-mokJS@users.noreply.github.com>
2022-09-07 11:20:23 +0200

    js update
    added some functionalities

--- script.js
+++ script.js
@@ -1,69 +1,54 @@
 //getting elements from DOM
 let addbutton = document.getElementById('submitButton');
-let deleteButton = document.getElementById('delete');
 let editButton = document.getElementById("edit");
 let form = document.getElementById('form');
 let listBox = document.getElementById('listBox');
 
-let myTodoList =[];
 
-//creating new list item 
-//add to dom
+//Litening to the submit buuton and calling the function to builds the element and prints to dom
+
+//event handlers
 form.addEventListener('submit', function(event){
     event.preventDefault();
-    doStuff();
+    addUserInput();
   });
-   
-  // building the to do list
+ 
 
-function doStuff(){
-
-  //empty out the list
-  // listBox.textContent = '';
-
+// building the to do list
+function addUserInput(){
   //building the items
-
   //fetching relevant items we will need
-  let myInput = document.getElementById('myInput').value;
+  let userInput = document.getElementById('myInput').value;
 
   // add classe to the fetched items
   let li = document.createElement('li');
   li.classList.add('list-item');
 
-  let spanEdit = document.createElement('button');
-  spanEdit.innerHTML = 'edit';
-  spanEdit.classList.add('edit');
-
-  let input = document.createElement('input');
-  input.classList.add('todo-item');
-  input.setAttribute('type', 'checkbox');
-
-  let spanDelete = document.createElement('button');
-  spanDelete.innerHTML = 'delete';
-  spanDelete.classList.add('delete');
-
-  let outPutText = document.createTextNode(myInput);
-
   //check if the user has added relevant info in to do list
-  if (myInput == '' || myInput == null){
+  if (userInput == '' || userInput == null){
 
     let error = document.getElementById('error');
     error.innerHTML = 'Please enter something to do.';
- } 
- else{
-// if user added all relevant things build the list and appen the children
+   } 
+     else{
+        // if user added all relevant things build the list and appen the children
+          listBox.appendChild(li);
+          li.innerHTML = `
+                        <input 
+                        class="todo-item"
+                        type="checkbox" 
+                        name="item" 
+                        id="todo-item">
+                        <input class="p-todo"
+                        type="text"
+                        value="${userInput}"
+                        readonly
+                        >
+                        <button name="edit" class="edit" id="edit">edit</button>
+                        <button name="delete" class="delete" id="delete">delete</button>
+          `
+          document.getElementById('myInput').value = '';
+        }
 
-  listBox.appendChild(li);
-  li.appendChild(input);
-  li.appendChild(outPutText,);
-  li.appendChild(spanEdit);
-  li.appendChild(spanDelete);
-
-
-
-  console.log(myInput);
-  document.getElementById('myInput').value = '';
- }
-
-  
 }
+
